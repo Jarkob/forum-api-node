1f317543904374fb9f52da9892e17e21c0d6799d
Jakob Bussas <jakobbu@gmx.de>
2018-05-06 21:41:46 +0200

    created controllers

---
+++ app/controllers/topicController.js
@@ -0,0 +1,60 @@
+var Topic = require('../models/topic');
+
+exports.getAll = function(req, res) {
+    Topic.find(function(err, topics) {
+        if(err) {
+            res.send(err);
+        }
+        res.json(topics);
+    });
+}
+
+exports.getById = function(req, res) {
+    Topic.findById(req.params.topic_id, function(err, topic) {
+        if(err) {
+            res.send(err);
+        }
+        res.json(topic);
+    });
+}
+
+exports.create = function(req, res) {
+    var topic = new Topic();
+    topic.title = req.body.title;
+
+    // save topic and check for errors
+    topic.save(function(err) {
+        if(err) {
+            res.send(err);
+        }
+        res.json({message: 'Topic created'});
+    });
+}
+
+exports.update = function(req, res) {
+    Topic.findById(req.params.topic_id, function(err, topic) {
+        if(err) {
+            res.send(err);
+        }
+        topic.title = req.body.title;
+
+        // save the topic
+        topic.save(function(err) {
+            if(err) {
+                res.send(err);
+            }
+            res.json({message: 'Topic updated'});
+        });
+    });
+}
+
+exports.delete = function(req, res) {
+    Topic.remove({
+        _id: req.params.topic_id
+    }, function(err, topic) {
+        if(err) {
+            res.send(err);
+        }
+        res.json({message: 'Topic deleted'});
+    })
+}
