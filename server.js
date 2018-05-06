4f873299b548d3cadb826d6dfc00d687287abe15
Jakob Bussas <jakobbu@gmx.de>
2018-05-06 21:59:00 +0200

    included router

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(bodyParser.json());
 var port = process.env.PORT || 8080; // set port
 
 // define routes
-var router = express.Router();
+var router = require('router');
 
 // middleware to use for all requests
 router.use(function(req, res, next) {
@@ -30,71 +30,6 @@ router.get('/', function(req, res) {
     res.json({message: 'api works'});
 });
 
-// more routes
-router.route('/topics')
-    // get all topics
-    .get(function(req, res) {
-        Topic.find(function(err, topics) {
-            if(err) {
-                res.send(err);
-            }
-            res.json(topics);
-        })
-    })
-
-    // create new topic
-    .post(function(req, res) {
-        var topic = new Topic();
-        topic.title = req.body.title;
-
-        // save topic and check for errors
-        topic.save(function(err) {
-            if(err) {
-                res.send(err);
-            }
-            res.json({message: 'Topic created'});
-        });
-    });
-
-router.route('/topics/:topic_id')
-    // get topic by id
-    .get(function(req, res) {
-        Topic.findById(req.params.topic_id, function(err, topic) {
-            if(err) {
-                res.send(err);
-            }
-            res.json(topic);
-        });
-    })
-
-    // update topic by id
-    .put(function(req, res) {
-        Topic.findById(req.params.topic_id, function(err, topic) {
-            if(err) {
-                res.send(err);
-            }
-            topic.title = req.body.title;
-
-            // save the topic
-            topic.save(function(err) {
-                if(err) {
-                    res.send(err);
-                }
-                res.json({message: 'Topic updated'});
-            });
-        });
-    })
-
-    .delete(function(req, res) {
-        Topic.remove({
-            _id: req.params.topic_id
-        }, function(err, topic) {
-            if(err) {
-                res.send(err);
-            }
-            res.json({message: 'Topic deleted'});
-        })
-    });
 
 app.use('/api', router);
 
