7eeb9ab825fee638fb4f62ea2b5e03db4f0a6aab
Jakob Bussas <jakobbu@gmx.de>
2018-08-30 21:35:12 +0200

    SECRET password lol

--- db.js
+++ db.js
@@ -1,4 +1,5 @@
 var mongoose = require('mongoose');
 
 // connect to db
-mongoose.connect('mongodb://127.0.0.1:27017/forum');
+// mongoose.connect('mongodb://127.0.0.1:27017/forum');
+mongoose.connect('mongodb://forum_user:Jakob123@cluster0-shard-00-00-ee4mc.mongodb.net:27017,cluster0-shard-00-01-ee4mc.mongodb.net:27017,cluster0-shard-00-02-ee4mc.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true/forum');
