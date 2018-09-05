760898e3502d179aa194a98e11ac5fbe4b38ee7c
Jakob Bussas <jakobbu@gmx.de>
2018-09-05 18:42:39 +0200

    added userId attribute to topic scheme so user authorization can be implemented

--- app/models/topic.js
+++ app/models/topic.js
@@ -2,6 +2,7 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var TopicSchema = new Schema({
+    userId: String,
     title: String,
     postCount: Number,
     lastPostId: String,
